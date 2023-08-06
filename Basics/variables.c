9f61b15568b35d67b1e14a18b5f18a5f06014aa8
leen-neel <and24903@gmail.com>
2023-08-06 21:06:35 +0530

    whoa variables

--- Basics/variables.c
+++ Basics/variables.c
@@ -2,6 +2,20 @@
 
 int main()
 {
-    int sex = 2;
-    printf("%d\n", sex);
+    int age = 18;
+
+    printf("Age of consent is %d\n", age);
+
+    int a = 0, b = 0, sum = 0;
+    printf("\nEnter a number : ");
+    scanf("%d", &a);
+
+    printf("Enter another number : ");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("Sum is: %d\n", sum);
+
+    return 0;
 }
