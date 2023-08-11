bbe7eb7eb464f72f7fc8009fe8a40aad86cd072a
leen-neel <and24903@gmail.com>
2023-08-11 22:56:04 +0530

    nice

--- Syntax/strings.c
+++ Syntax/strings.c
@@ -5,5 +5,11 @@ int main()
     char name[] = "Neel";
     printf("Helloooo, my name is %s\n", name);
 
+    // take string input
+    char name2[20];
+    printf("\nEnter your name: ");
+    scanf("%s", name2);
+    printf("Helloooo, my name is %s\n", name2);
+
     return 0;
 }
