34bac4c0f5a183f371dc08319693603352ea50a8
leen-neel <and24903@gmail.com>
2023-08-23 22:24:27 +0530

    added a way to run C++

--- run.py
+++ run.py
@@ -7,5 +7,7 @@ path = sys.argv[1]
 
 if path.endswith(".c") and os.path.exists(path):
     os.system(f"gcc {path} && ./a.out && rm a.out")
+elif path.endswith(".cpp") and os.path.exists(path):
+    os.system(f"g++ {path} && ./a.out && rm a.out")
 else:
     print("Invalid file")
