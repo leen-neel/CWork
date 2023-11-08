11291c4703780c7eae98c88b4a160b83b11fb781
leen-neel <and24903@gmail.com>
2023-11-08 10:38:44 +0530

    idk

--- WeirdThings/Floats.c
+++ WeirdThings/Floats.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 
     float f = 0.1;
-    printf("%d\n", f == 0.1); // prints 0 (false)
-    printf("%s", 0.1 == 0.1); // prints 1 (true)
+    printf("%d\n", f == 0.1);   // prints 0 (false)
+    printf("%d\n", 0.1 == 0.1); // prints 1 (true)
+
+    float tolerance = 1e-6; // Define a small tolerance 1 x 10^-6
+
+    if (fabs(f - 0.1) < tolerance)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
 
     return 0;
 }
