c000d25a8cba78db4d5043344153ca39027868ea
leen-neel <and24903@gmail.com>
2024-01-19 00:26:59 +0530

    primed

---
+++ 1st-ESA/10-PrimeChecker.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 0, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // check if num is a prime or not
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+    {
+        printf("\n%d is a prime number.\n", num);
+    }
+    else
+    {
+        printf("\n%d is not a prime number.\n", num);
+    }
+}
