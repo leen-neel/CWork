65f1d9b7d21dbcde91093c3799c29892390a8a02
leen-neel <and24903@gmail.com>
2024-01-19 09:11:00 +0530

    measurer

---
+++ 1st-ESA/19-StringLength.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+        ;
+
+    printf("Length of string: %d\n", i);
+
+    return 0;
+}
