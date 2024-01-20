7a6ab33e119b1c7e804fa174d624dded6ebc6d2d
leen-neel <and24903@gmail.com>
2024-01-20 09:01:10 +0530

    fixed shit

--- 1st-ESA/19-StringLength.c
+++ 1st-ESA/19-StringLength.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -8,6 +9,8 @@ int main()
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
 
     for (i = 0; str[i] != '\0'; i++)
         ;
